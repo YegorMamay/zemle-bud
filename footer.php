da06bf80c480d399048bdb6a9bc58b2f4428a440
Pavel <ponomarenko.pavel.web@gmail.com>
2020-02-24 16:30:07 +0200

    style for footer

--- footer.php
+++ footer.php
@@ -1,19 +1,46 @@
 <footer class="footer js-footer">
-    <?php if (is_active_sidebar('footer-widget-area')) : ?>
-        <div class="pre-footer">
-            <div class="container">
-                <div class="row">
-                    <?php dynamic_sidebar('footer-widget-area'); ?>
+    <div class="footer__wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-sm-12 col-md-12 col-lg-2 col-xl-2">
+                    <div class="logo">
+                        <?php get_default_logo_link([
+                            'name'    => 'logo.svg',
+                            'options' => [
+                                'class'  => 'logo-img',
+                                'width'  => 150,
+                                'height' => 50,
+                            ],
+                        ])
+                        ?>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-12 col-md-12 col-lg-10 col-xl-10">
+                    <div class="footer__container">
+                        <?php if (has_phones()) { ?>
+                            <ul class="phone">
+                                <?php foreach (get_phones() as $phone) { ?>
+                                    <li class="phone-item">
+                                        <span class="phone-icon">
+                                            <i class="far fa-phone-alt"></i>
+                                        </span>
+                                        <a href="tel:<?php echo esc_attr(get_phone_number($phone)); ?>" class="phone-number">
+                                            <?php echo esc_html($phone); ?>
+                                        </a>
+                                    </li>
+                                <?php } ?>
+                            </ul>
+                        <?php } ?>
+                    </div>
                 </div>
             </div>
-        </div><!-- .pre-footer end-->
-    <?php endif; ?>
-
+        </div>
+    </div>
     <div class="container">
         <div class="copyright">
             <div class="date">&copy; <?php echo date('Y'); ?>. Все права защищены</div>
             <div class="developer">
-                <?php _e('Developed by ', 'brainworks') ?><a href="https://brainworks.pro/" target="_blank">BRAIN WORKS</a> 
+                <?php _e('Developed by ', 'brainworks') ?><a href="https://brainworks.pro/" target="_blank">BrainWorks</a>
             </div>
         </div>
     </div>
