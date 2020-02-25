05c89f03771a7eec9045f8818588ceb42de4ef1a
Pavel <ponomarenko.pavel.web@gmail.com>
2020-02-25 15:03:09 +0200

    block prices, style for horizontal form

--- page-home.php
+++ page-home.php
@@ -51,9 +51,9 @@ $advantages_elem_id = get_post_meta(get_the_ID(), 'advantages_image', true);
 $advantages_image = wp_get_attachment_url($advantages_elem_id);
 $advantages_content = get_field('advantages_content');
 ?>
-<section class="advantages-section">
+<section class="advantages-section" id="advantages">
     <div class="container">
-        <h2 class="advanced-section__title main-title h2 text-center"><?php echo $advantages_content['advantages_title']; ?></h2>
+        <h2 class="advantages-section__title main-title h2 text-center"><?php echo $advantages_content['advantages_title']; ?></h2>
         <div class="advantages-section__description h2 text-center"><?php echo $advantages_content['advantages_description']; ?></div>
     </div>
     <div class="advantages-section__container" style="background: url('<?php echo $advantages_image; ?>') no-repeat bottom / cover">
@@ -109,14 +109,43 @@ $special_top = get_field('special_top');
                 <h2 class="block-special__description"><?php echo $special_top['special_description']; ?></h2>
             </div>
             <div class="block-special__item">
-                <button type="button" class="btn btn-primary"><?php echo $special_top['special_text_button']; ?></button>
+                <button type="button" class="btn btn-primary js-special-top"><?php echo $special_top['special_text_button']; ?></button>
             </div>
         </div>
     </div>
 </section>
-<div class="container">
-
-<?php get_template_part('loops/content', 'home'); ?>
-
-</div><!-- /.container -->
+<section class="block-prices" id="prices">
+    <div class="container">
+        <h2 class="main-title"><?php echo get_post_meta(get_the_ID(), 'price_title', true); ?></h2>
+        <?php get_template_part('loops/content', 'home'); ?>
+        <div class="block-prices__form-wrapper" id="price-form">
+            <img class="block-prices__image" src="/wp-content/themes/zemle-bud/assets/img/price.png" alt="decor image">
+            <div class="block-prices__form">
+                <div class="block-prices__form-title">
+                    <?php echo get_post_meta(get_the_ID(), 'price_form_title', true); ?>
+                    <img class="block-prices__arrow" src="/wp-content/themes/zemle-bud/assets/img/arrow-form.svg" alt="arrow">
+                </div>
+                <?php echo do_shortcode('[fc id=\'3\'][/fc]'); ?>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+$special_bottom = get_field('special_bottom');
+?>
+<section class="block-special" style="background: url('<?php echo $special_bottom['special_image']; ?>') no-repeat center / cover">
+    <div class="container">
+        <div class="block-special__wrapper">
+            <div class="block-special__item">
+                <div class="block-special__text-wrapper">
+                    <div class="block-special__text"><?php echo $special_bottom['special_title']; ?></div>
+                </div>
+                <h2 class="block-special__description"><?php echo $special_bottom['special_description']; ?></h2>
+            </div>
+            <div class="block-special__item">
+                <button type="button" class="btn btn-primary js-special-bottom"><?php echo $special_bottom['special_text_button']; ?></button>
+            </div>
+        </div>
+    </div>
+</section>
 <?php get_footer(); ?>
